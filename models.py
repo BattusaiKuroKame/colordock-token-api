49129dc1c60fb72e074ff8acbfdd0cad1fe5d868
Battusai Kuro Kame <69150947+BattusaiKuroKame@users.noreply.github.com>
2025-12-10 12:57:42 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 class LoginRequest(BaseModel):
-    email: EmailStr
+    email: str  # ← Changed from EmailStr
     password: str
 
 class LoginResponse(BaseModel):
